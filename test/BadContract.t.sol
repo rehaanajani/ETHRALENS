9e0dee38dfe70019958a07f7f58e51fbdd659f67
Rehaan <your@email.com>
2026-04-16 04:36:41 +0530

    demo: high gas contract

---
+++ test/BadContract.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+contract Bad {
+    uint public value;
+
+    function badSet(uint x) public {
+        for (uint i = 0; i < 20; i++) {
+            value = x;
+        }
+    }
+}
+
+contract BadTest is Test {
+    Bad bad;
+
+    function setUp() public {
+        bad = new Bad();
+    }
+
+    function test_BadSet() public {
+        bad.badSet(10);
+    }
+}
